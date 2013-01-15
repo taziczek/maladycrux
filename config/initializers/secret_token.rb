b6e3997e6782ff25f8774eba7c50c6ae0b675be2
tazik <tazik@vk1072.megiteam.com.pl>
2013-01-15 20:11:03 +0100

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maladycrux::Application.config.secret_token = '0cb3bfa27efa27fdbdf1cc562da184dfe4b7678f43e03cf7e4f3c8e7fd20e1d356f53d27076de698d7b3b06e2e44ca1732932fc5d3abf04995c28a12fa0976d8'
